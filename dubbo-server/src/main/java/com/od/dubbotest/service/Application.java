1562f1a45d277afd9d8a83d03793c5cb3c02fc07
xy1219.zhao <xy1219.zhao@samsung.com>
2020-06-05 09:29:46 +0000

    apollo分支添加Logger

--- dubbo-server/src/main/java/com/od/dubbotest/service/Application.java
+++ dubbo-server/src/main/java/com/od/dubbotest/service/Application.java
@@ -1,16 +1,18 @@
 package com.od.dubbotest;
 
 import java.io.IOException;
-
+import java.util.logging.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    public static void main( String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run("classpath*:spring-config.xml",args);
-        System.out.println("Dubbo server started");
-        System.out.println("Dubbo 服务端已经启动");
+        Logger log = Logger.getLogger("com"); 
+        log.info("Dubbo server started");
+        log.info("Dubbo 服务端已经启动");
+        log.info("Dubbo 等待输出");
         while(true) {
             System.in.read();
         }
