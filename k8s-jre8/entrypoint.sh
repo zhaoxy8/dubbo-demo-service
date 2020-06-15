027421b6187e73e29f358aae10cb00044bb830ac
xy1219.zhao <xy1219.zhao@samsung.com>
2020-06-15 06:48:01 +0000

    delete jenkins

--- k8s-jre8/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-M_OPTS="-Duser.timezone=Asia/Shanghai -javaagent:/opt/prom/jmx_prometheus_javaagent-0.12.0.jar=$(hostname -i):${M_PORT:-"12346"}:/opt/prom/config.yml"
-C_OPTS=${C_OPTS}
-JAR_BALL=${JAR_BALL}
-exec java -jar ${M_OPTS} ${C_OPTS} ${JAR_BALL}
